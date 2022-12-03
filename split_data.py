b02e9fe726afcd74c69e0213f4b50e6d6c5b8b42
Marci0707 <schneider.marcell2@gmail.com>
2022-12-03 12:14:00 +0100

    data splits

---
+++ split_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def main():
+    df = pd.read_csv('./data/bp_weather_data.csv')
+    TEST_RATIO = 0.2
+
+    cut_idx = int(len(df)*(1-TEST_RATIO))
+
+    test_df = df.iloc[:cut_idx]
+    train_df = df.iloc[cut_idx:]
+
+    train_df.to_csv('./data/train_data.csv',index=False)
+    test_df.to_csv('./data/test_data.csv',index=False)
+
+
+if __name__ == '__main__':
+    main()
